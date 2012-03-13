5a217f197a71bf26cd1719acb1981899515a1262
Antonio El Khoury <aelkhour@laas.fr>
2012-03-13 23:14:42 +0100

    Add capsule geometry during creation of component.

--- src/component/capsule.cc
+++ src/component/capsule.cc
@@ -242,6 +242,13 @@ namespace hpp
 	  {
 	    ptrShPtr.reset ();
 	  }
+	else
+	  {
+	    CkcdPoint e1 (- height / 2, 0, 0);
+	    CkcdPoint e2 (height / 2, 0, 0);
+	    ptrShPtr->addCapsule (e1, e2, radius);
+	    ptrShPtr->makeCollisionEntity (CkcdObject::IMMEDIATE_BUILD);
+	  }
 
 	return ptrShPtr;
       }
